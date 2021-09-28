4c121e9a0bdb0cd580204a82a490f27297937b79
pyrish <mariano.vazquez@live.com>
2021-09-28 07:17:33 +0100

    added functionality to AccountNavigate

--- TaskSetLib/MyAccountNavigate.py
+++ TaskSetLib/MyAccountNavigate.py
@@ -1,11 +1,21 @@
 from locust import task, SequentialTaskSet
+from CommonLib.UtilHelper import UtilHelper
 
 
 class MyAccountNavigate(SequentialTaskSet):
 
     @task
     def fetch_personal_information(self):
-        pass
+        # I can access methods from AbstractUser by doing 'self.user.get_cookie()'
+        # since RegisteredHttpUser(on start) already grabbed the cookies and RegisteredHttpUser
+        # inherits from AbstractUser
+        header = UtilHelper.get_base_header_with_cookie(self.user.get_cookie())
+        with self.client.get("/index.php?controller=addresses", headers=header, catch_response=True) as response:
+            if response.status_code == 200:
+                Logger.log_message("fetch my-account user address" + self.user.get_email())
+            else:
+                response.failure("Failed to fetch my-account user address" + self.user.get_email())
+                Logger.log_message("Failed to fetch my-account user address" + self.user.get_email(), LogType.ERROR)
 
     @task
     def exit_task_execution(self):
