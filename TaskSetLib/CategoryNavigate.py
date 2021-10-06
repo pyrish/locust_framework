c252f51db937dbc81ffe70a2e31f25fd09b1c5ab
pyrish <mariano.vazquez@live.com>
2021-10-06 06:56:39 +0100

    added status code reference in log message

--- TaskSetLib/CategoryNavigate.py
+++ TaskSetLib/CategoryNavigate.py
@@ -13,21 +13,21 @@ class CategoryNavigate(SequentialTaskSet):
         with self.client.get("/index.php?id_category=3&controller=category", headers=self.header, catch_response=True) as response:
             if response.status_code != 200:
                 response.failure("Failed to navigate to women category, EXCEPTION" + response.text)
-                Logger.log_message("Failed to navigate to women category", LogType.ERROR)
+                Logger.log_message("Failed to navigate to women category, Status Code-" + str(response.status_code), LogType.ERROR)
 
     @task
     def navigate_to_dresses_category(self):
         with self.client.get("/index.php?id_category=8&controller=category", headers=self.header, catch_response=True) as response:
             if response.status_code != 200:
                 response.failure("Failed to navigate to dresses category, EXCEPTION" + response.text)
-                Logger.log_message("Failed to navigate to dresses category", LogType.ERROR)
+                Logger.log_message("Failed to navigate to dresses category, Status Code-" + str(response.status_code), LogType.ERROR)
 
     @task
     def navigate_to_shirt_category(self):
         with self.client.get("/index.php?id_category=5&controller=category", headers=self.header, catch_response=True) as response:
             if response.status_code != 200:
                 response.failure("Failed to navigate to shirt category, EXCEPTION" + response.text)
-                Logger.log_message("Failed to navigate to shirt category", LogType.ERROR)
+                Logger.log_message("Failed to navigate to shirt category, Status Code-" + str(response.status_code), LogType.ERROR)
 
     @task
     def exit_task_execution(self):
